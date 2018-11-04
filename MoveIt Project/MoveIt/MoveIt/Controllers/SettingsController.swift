c319862687665e341305290acfca4b124245dc5b
Yifan Li <yl506@duke.edu>
2018-11-03 22:40:38 -0400

    Add Project ver0.1

---
+++ MoveIt Project/MoveIt/MoveIt/Controllers/SettingsController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsController.swift
+//  MoveIt
+//
+//  Created by Haohong Zhao on 11/2/18.
+//  Copyright © 2018 MoveIt. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()    }
+}
